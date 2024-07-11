76c28b6d5bdd93862aae7561fc1d5d060cdf95a4
shun <shunshunshun0820@icloud.com>
2024-07-11 22:18:40 +0900

    大項目が増えるロジック※削除した後増やすと問題が起きる

--- createWBS/src/Components/Create.js
+++ createWBS/src/Components/Create.js
@@ -1,9 +1,73 @@
 import { Link } from "react-router-dom";
 import { useWbs } from "../Hooks/use-context";
+import { useState } from "react";
 
 // 作成したWBSの表示を行う
 const Create = () => {
-    const { a,c } = useWbs();
+    const { a, c } = useWbs();
+    const [taskId, setTaskId] = useState(0);
+    const [tasks, setTasks] = useState([0]);
+
+    const addTasks = () => {
+        setTasks([...tasks, taskId + 1]);
+        setTaskId(taskId+1);
+    };
+    const removeElement =(elementId)=> { 
+        let indexToRemove = tasks.indexOf(elementId+1);
+    
+    if (indexToRemove !== -1) {
+        tasks.splice(indexToRemove, 1);
+    }
+        // 指定されたIDを持つ要素を取得
+        var element = document.getElementById("task"+elementId);
+        // 要素が存在する場合、削除
+        if (element) {
+            element.parentNode.removeChild(element);
+        }
+    }
+    const BigTask = (props) => {
+
+        return (
+            <>
+                <tr id={"task"+props.id}>
+                    <input type="text" />
+                    
+                    <div>
+                        <button button onClick={() =>removeElement(props.id)}>削除</button>
+                    </div>
+                    <td>
+
+                        <SmallTask />
+                        <SmallTask />
+                        <SmallTask />
+                        <SmallTask />
+
+                    </td>
+                    <td>
+                        <WorkingTime />
+                    </td>
+                </tr>
+            </>
+        )
+    }
+
+    const SmallTask = () => {
+        return (
+            <div>
+                <input type="text"></input>
+                <button>削除</button>
+            </div>
+        )
+    }
+
+    const WorkingTime = () => {
+        return (
+            <>
+                <input type="number"></input>
+            </>
+        )
+    }
+
     return (
         <>
             <h1>WBS作成画面</h1>
@@ -15,38 +79,22 @@ const Create = () => {
             <Link to={`/`}>
                 <button>トップへ戻る</button>
             </Link>
+            <div>
+                        <button onClick={addTasks}>増えます</button>
+                    </div>
 
             <table>
                 <tr>
                     <th>大項目</th>
                     <th>小項目</th>
                     <th>工数</th>
-                    <th>開始日付</th>
-                    <th>終了日付</th>
-                    <th>一日の工数</th>
+
                 </tr>
-                <tr>
-                    <td>Data 1</td>
-                    <td>
-                        <textarea id="comments" name="comments"></textarea>
-                        <button>削除</button>
-                    </td>
-                    <td>
-                        <textarea id="comments" name="comments"></textarea>
-                        <button>削除</button>
-                    </td>
-                    <td>Data 3</td>
-                </tr>
-                <tr>
-                    <td>Data 4</td>
-                    <td>Data 5</td>
-                    <td>Data 6</td>
-                </tr>
-                <tr>
-                    <td>Data 7</td>
-                    <td>Data 8</td>
-                    <td>Data 9</td>
-                </tr>
+                {tasks.map((task, index) => (
+          <> <BigTask id={task} /></>
+        ))}
+               
+
             </table>
         </>
     );
